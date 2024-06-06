1bbe6c848379439070553190e5e1ced42ea877a3
Emilie Montpre <SakoSuta.em@gmail.com>
2024-06-06 03:39:18 +0200

    Update AppNavigator to use bottom tab navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={HomeScreen} />
+      <Tab.Screen name="zd" component={HomeScreen} />
+      <Tab.Screen name="Settiqdzzdngs" component={HomeScreen} />
+    </Tab.Navigator>
   );
 };
 
