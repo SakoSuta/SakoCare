1ca01ab29e547652cb42cd4b019c856a32846b94
Emilie Montpre <SakoSuta.em@gmail.com>
2024-07-03 17:03:24 +0200

    Update AccountScreen section titles and icons

--- screens/AccountScreen/AccountScreen.js
+++ screens/AccountScreen/AccountScreen.js
@@ -10,6 +10,13 @@ import colors from '../../styles/colors';
 import styles from './styles';
 
 import user from '../../assets/icons/Account_Page/user.png';
+import subscription from '../../assets/icons/Account_Page/crown.png';
+import notification from '../../assets/icons/Account_Page/notification.png';
+import favorite from '../../assets/icons/Account_Page/favorite-day.png';
+import album from '../../assets/icons/Account_Page/album.png';
+import FAQ from '../../assets/icons/Account_Page/FAQ.png';
+import Contact from '../../assets/icons/Account_Page/Contact.png';
+import Star from '../../assets/icons/Account_Page/Star.png';
 import BackButton from '../../components/BackButton/BackButton';
 
 const AccountScreen = ({ navigation }) => {
@@ -50,69 +57,62 @@ const AccountScreen = ({ navigation }) => {
             </View>
           </View>
           <View style={styles.SectionContainer}>
-            <Text style={styles.SectionTitle}>Account</Text>
+            <Text style={styles.SectionTitle}>Personalization</Text>
             <View>
               <Button
-                title="Account"
+                title="Subscription"
                 onPress={() => navigation.navigate('EditProfile')}
                 color={colors.backgroundElement}
                 variant='alternate-1'
-                icon={user}
+                icon={subscription}
               />
               <Button
-                title="Account"
+                title="Notifications"
                 onPress={() => navigation.navigate('EditProfile')}
                 color={colors.backgroundElement}
                 variant='alternate-2'
-                icon={user}
+                icon={notification}
               />
               <Button
-                title="Account"
+                title="Favorites Days"
+                onPress={() => navigation.navigate('EditProfile')}
+                color={colors.backgroundElement}
+                variant='alternate-2'
+                icon={favorite}
+              />
+              <Button
+                title="Album Photos"
                 onPress={() => navigation.navigate('EditProfile')}
                 color={colors.backgroundElement}
                 variant='alternate-3'
-                icon={user}
+                icon={album}
               />
               <Shadow size="Normal" color={colors.primary} />
             </View>
           </View>
           <View style={styles.SectionContainer}>
-            <Text style={styles.SectionTitle}>Account</Text>
+            <Text style={styles.SectionTitle}>Help</Text>
             <View>
               <Button
-                title="Account"
+                title="FAQ"
                 onPress={() => navigation.navigate('EditProfile')}
                 color={colors.backgroundElement}
                 variant='alternate-1'
-                icon={user}
+                icon={FAQ}
               />
               <Button
-                title="Account"
+                title="Contact Us"
                 onPress={() => navigation.navigate('EditProfile')}
                 color={colors.backgroundElement}
                 variant='alternate-2'
-                icon={user}
+                icon={Contact}
               />
               <Button
-                title="Account"
-                onPress={() => navigation.navigate('EditProfile')}
-                color={colors.backgroundElement}
-                variant='alternate-2'
-                icon={user}
-              />
-              <Button
-                title="Account"
-                onPress={() => navigation.navigate('EditProfile')}
-                color={colors.backgroundElement}
-                variant='alternate-2'
-                icon={user}
-              />
-              <Button
-                title="Account"
+                title="Write a Review"
                 onPress={() => navigation.navigate('EditProfile')}
                 color={colors.backgroundElement}
                 variant='alternate-3'
-                icon={user}
+                icon={Star}
               />
               <Shadow size="Normal" color={colors.primary} />
             </View>
